3ca9d45948ac289b904f393ade37925d630eadef
Leonardo Tramontini <leo.tramontini@gmail.com>
2021-02-12 15:16:00 -0300

    feat: adding looping

--- hello/hello.go
+++ hello/hello.go
@@ -8,11 +8,20 @@ import (
 )
 
 func main() {
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
+	// log.SetPrefix("greetings: ")
+	// log.SetFlags(0)
 
-	// Get a greeting message and print it.
-	message, erro := greetings.Hello("Dylan")
+	// // Get a greeting message and print it.
+	// message, erro := greetings.Hello("Dylan")
+
+	// if erro != nil {
+	// 	log.Println(erro)
+	// }
+
+	// fmt.Println(message)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	message, erro := greetings.Hellos(names)
 
 	if erro != nil {
 		log.Println(erro)
