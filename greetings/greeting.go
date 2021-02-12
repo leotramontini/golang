a3657f56e2871b7f55141c94643a7a0832c1df4a
Leonardo Tramontini <leo.tramontini@gmail.com>
2021-02-12 15:36:29 -0300

    feat: adding tests

--- greetings/greeting.go
+++ greetings/greeting.go
@@ -15,7 +15,8 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf(randomFormat(), name)
+	// message := fmt.Sprintf(randomFormat(), name)
+	message := fmt.Sprint(randomFormat())
 	return message, nil
 }
 
